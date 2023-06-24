d98263a83c25516a1656bc95e288ef2c93a91bbc
Jefferson Pedro <jeffersonpedro89@hotmail.com>
2023-06-24 16:59:16 -0300

    emitindo eventos entre cliente e servidor

--- public/documento.js
+++ public/documento.js
@@ -1,1 +1,7 @@
 const socket = io();
+
+const textoEditor = document.getElementById('editor-texto');
+
+textoEditor.addEventListener('keyup', () => {
+    socket.emit('texto-editor', textoEditor.value);
+});
--- src/socket-backend.js
+++ src/socket-backend.js
@@ -2,4 +2,8 @@ import io from "./servidor.js";
 
 io.on('connection', (socket) => {
     console.log('Um cliente se conectou! ID:', socket.id);
+
+    socket.on('texto-editor', (texto) => {
+        console.log(texto);
+    });
 });
