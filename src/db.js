76d68655940babde61f32370e68e9714c6c1a0e0
Jefferson Pedro <jeffersonpedro89@hotmail.com>
2024-01-23 18:36:36 -0300

    configura o driver do mongodb

---
+++ src/db.js
@@ -0,0 +1,20 @@
+import { MongoClient } from "mongodb";
+
+const uri = "mongodb+srv://alura:alura@aluracluster.ogorlvu.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+
+async function run() {
+    try {
+        const database = client.db('alura-websocket');
+        const documentos = database.collection('documentos');
+
+        const query = { nome: 'JavaScript' };
+        const documento = await documentos.findOne(query);
+
+        console.log(documento);
+    } finally {
+        await client.close();
+    }
+}
+
+run().catch(console.dir);
