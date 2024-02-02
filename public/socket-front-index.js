1fd2203fdb94d6c123cdf1875fb4be84dfa078e5
Jefferson Pedro <jeffersonpedro89@hotmail.com>
2024-02-02 16:17:16 -0300

    trata inserção de documento existente

--- public/socket-front-index.js
+++ public/socket-front-index.js
@@ -16,4 +16,8 @@ socket.on('adicionar_documento_interface', (nome) => {
   inserirLinkDocumento(nome);
 });
 
+socket.on('documento_existente', (nome) => {
+  alert(`O documento ${nome} já existe!`);
+});
+
 export { emitirAdicionarDocumento };
