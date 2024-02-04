f146fd9175e700619c2de489cfa341e19b442a5c
Jefferson Pedro <jeffersonpedro89@hotmail.com>
2024-02-03 21:01:21 -0300

    criptografa senha dos usuários cadastrados

--- servidor/db/usuariosDb.js
+++ servidor/db/usuariosDb.js
@@ -1,14 +1,17 @@
+import criaHashESalSenha from "../utils/criaHashESalSenha.js";
 import { usuariosColecao } from "./dbConnect.js";
 
 function cadastrarUsuario({ nome, senha }) {
-  return usuariosColecao.insertOne({ nome, senha });
+  const { hashSenha, salSenha } = criaHashESalSenha(senha);
+
+  return usuariosColecao.insertOne({ nome, hashSenha, salSenha });
 }
 
 function encontrarUsuario(nome) {
   return usuariosColecao.findOne({ nome });
 }
 
-export { 
+export {
   cadastrarUsuario,
   encontrarUsuario
- }
+}
