0eff1255f75077666f57651abf5b3a53611bd720
Jefferson Pedro <jeffersonpedro89@hotmail.com>
2024-02-09 17:44:00 -0300

    refatora a autenticação de usuário

--- servidor/registrarEventos/login.js
+++ servidor/registrarEventos/login.js
@@ -1,10 +1,21 @@
-import autenticaUsuario from "../utils/autenticaUsuario.js";
+import autenticarUsuario from "../utils/autenticarUsuario.js";
+import { encontrarUsuario } from "../db/usuariosDb.js";
 
 function registrarEventosLogin(socket, io) {
   socket.on('autenticar_usuario', async ({ nome, senha }) => {
-    const autenticado = await autenticaUsuario(nome, senha)
+    const usuario = await encontrarUsuario(nome);
 
-    console.log(autenticado);
+    if (usuario) {
+      const autenticado = await autenticarUsuario(senha, usuario);
+
+      if (autenticado) {
+        socket.emit('autenticacao_sucesso');
+      } else {
+        socket.emit('autenticacao_erro');
+      }
+    } else {
+      socket.emit('usuario_nao_encontrado');
+    }
   });
 }
 
